23062d943ddd43f7c7f277ab6b9a39e9c02ca3f6
Patrick John Kelly <G00307732@gmit.ie>
2017-11-17 11:34:07 +0000

    Testing Loops and Files

--- Test.py
+++ Test.py
@@ -1,9 +1,4 @@
-Teams = ['Arsenal', 'Aston Villa', 'Barnsley', 'Birmingham', 'Blackburn', 'Blackpool', 'Bolton', 'Bournemouth',
-             'Bradford', 'Brighton', 'Burnley', 'Cardiff', 'Charlton', 'Chelsea', 'Coventry', 'Crystal Palace', 'Derby',
-             'Everton', 'Fulham', 'Huddersfield', 'Hull', 'Ipswich', 'Leeds', 'Leicester', 'Liverpool', 'Man City',
-             'Man United', 'Middlesbrough', 'Newcastle', 'Norwich', 'Oldham', 'Portsmouth', 'QPR', 'Reading',
-             'Sheffield United', 'Sheffield Weds', 'Southampton', 'Stoke', 'Sunderland', 'Swansea', 'Swindon', 'Tottenham',
-             'Watford', 'West Brom', 'West Ham', 'Wigan', 'Wimbledon', 'Wolves']
+Teams = ['Arsenal', 'Chelsea']
 
 beatenTeam=[]
 lostToTeams=[]
@@ -13,32 +8,36 @@ loss = 0
 draw=0
 
 seasonResults = open('testfile.txt', 'r')
-for line in seasonResults:
-    home, away, result ,newline = line.split(',')
-    if home == 'Arsenal':
-        if result == 'H':
-            beatenTeam.append(away)
-            win += 1
-        elif result == 'A':
-            lostToTeams.append(away)
-            loss += 1
-        elif result == 'D':
-            drewWith.append(away)
-            draw += 1
-    elif away == 'Arsenal':
-        if result == 'A':
-            beatenTeam.append(home)
-            win += 1
-        elif result == 'H':
-            lostToTeams.append(home)
-            loss += 1
-        elif result == 'D':
-            drewWith.append(home)
-            draw += 1
 
-    print("beaten Teams")
-    print(beatenTeam)
-    print("Lost To")
-    print(lostToTeams)
-    print("drew with")
-    print(drewWith)
+
+for teamName in Teams:
+
+    for line in seasonResults:
+        teamFiles = open("{}.txt".format(teamName), "w")
+        home, away, result ,newline = line.split(',')
+        if home == teamName:
+            if result == 'H':
+                beatenTeam.append(away)
+                win += 1
+            elif result == 'A':
+                lostToTeams.append(away)
+                loss += 1
+            elif result == 'D':
+                drewWith.append(away)
+                draw += 1
+        elif away == teamName:
+            if result == 'A':
+                beatenTeam.append(home)
+                win += 1
+            elif result == 'H':
+                lostToTeams.append(home)
+                loss += 1
+            elif result == 'D':
+                drewWith.append(home)
+                draw += 1
+        teamFiles.write("Win count =" + str(win) + "\n")
+        teamFiles.write("Loss count =" + str(loss) + "\n")
+        teamFiles.write("Draw Count =" + str(draw) + "\n")
+        teamFiles.write("beaten Teams =" + str(beatenTeam) + "\n")
+        teamFiles.write("Lost to Teams = " + str(lostToTeams) + "\n")
+        teamFiles.write("drew with Teams = " + str(drewWith) + "\n")
