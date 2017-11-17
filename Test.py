7b3694b7b91fdc64dda0fd38f850bfffa2b4f2a4
Patrick John Kelly <G00307732@gmit.ie>
2017-11-17 11:44:21 +0000

    Fixed Loopp Issue

--- Test.py
+++ Test.py
@@ -1,55 +1,78 @@
-Teams = ['Arsenal', 'Aston Villa', 'Barnsley', 'Birmingham', 'Blackburn', 'Blackpool', 'Bolton', 'Bournemouth',
-             'Bradford', 'Brighton', 'Burnley', 'Cardiff', 'Charlton', 'Chelsea', 'Coventry', 'Crystal Palace', 'Derby',
-             'Everton', 'Fulham', 'Huddersfield', 'Hull', 'Ipswich', 'Leeds', 'Leicester', 'Liverpool', 'Man City',
-             'Man United', 'Middlesbrough', 'Newcastle', 'Norwich', 'Oldham', 'Portsmouth', 'QPR', 'Reading',
-             'Sheffield United', 'Sheffield Weds', 'Southampton', 'Stoke', 'Sunderland', 'Swansea', 'Swindon', 'Tottenham',
-             'Watford', 'West Brom', 'West Ham', 'Wigan', 'Wimbledon', 'Wolves']
+import csv
+import requests
 
-beatenTeam = []
-lostToTeams = []
-drewWith = []
-win = 0
-loss = 0
-draw = 0
+URL = 'http://www.football-data.co.uk/mmz4281/1617/E0.csv'
 
-seasonResults = open('testfile.txt', 'r')
+with requests.Session() as s:
+    download = s.get(URL)
+    decoded_content = download.content.decode('utf-8')
 
-for teamName in Teams:
-    teamFiles = open("{}.txt".format(teamName), "w")
-    seasonResults = open('testfile.txt', 'r')
+    cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+    my_list = list(cr)
 
-    for line in seasonResults:
-        home, away, result, newline = line.split(',')
-        if home == teamName:
-            if result == 'H':
-                beatenTeam.append(away)
-                win += 1
-            elif result == 'A':
-                lostToTeams.append(away)
-                loss += 1
-            elif result == 'D':
-                drewWith.append(away)
-                draw += 1
-        elif away == teamName:
-            if result == 'A':
-                beatenTeam.append(home)
-                win += 1
-            elif result == 'H':
-                lostToTeams.append(home)
-                loss += 1
-            elif result == 'D':
-                drewWith.append(home)
-                draw += 1
-    teamFiles.write("Win count =" + str(win) + "\n")
-    teamFiles.write("Loss count =" + str(loss) + "\n")
-    teamFiles.write("Draw Count =" + str(draw) + "\n")
-    teamFiles.write("beaten Teams =" + str(beatenTeam) + "\n")
-    teamFiles.write("Lost to Teams = " + str(lostToTeams) + "\n")
-    teamFiles.write("drew with Teams = " + str(drewWith) + "\n")
-    win = 0
-    loss = 0
-    draw = 0
-    beatenTeam = []
-    lostToTeams = []
-    drewWith = []
-    teamFiles.close()
+    for row in my_list:
+        with open('testfile.txt', 'a') as f:
+            f.write(''.join(row[2] + ',' + row[3] + ',' + row[6] + ',') + '\n')
+
+    with open('testfile.txt', 'r') as f:
+        Teams = ['Arsenal', 'Aston Villa', 'Barnsley', 'Birmingham', 'Blackburn', 'Blackpool', 'Bolton', 'Bournemouth',
+                 'Bradford', 'Brighton', 'Burnley', 'Cardiff', 'Charlton', 'Chelsea', 'Coventry', 'Crystal Palace',
+                 'Derby',
+                 'Everton', 'Fulham', 'Huddersfield', 'Hull', 'Ipswich', 'Leeds', 'Leicester', 'Liverpool', 'Man City',
+                 'Man United', 'Middlesbrough', 'Newcastle', 'Norwich', 'Oldham', 'Portsmouth', 'QPR', 'Reading',
+                 'Sheffield United', 'Sheffield Weds', 'Southampton', 'Stoke', 'Sunderland', 'Swansea', 'Swindon',
+                 'Tottenham',
+                 'Watford', 'West Brom', 'West Ham', 'Wigan', 'Wimbledon', 'Wolves']
+
+        beatenTeam = []
+        lostToTeams = []
+        drewWith = []
+        win = 0
+        loss = 0
+        draw = 0
+
+        seasonResults = open('testfile.txt', 'r')
+
+        for teamName in Teams:
+            teamFiles = open("{}.txt".format(teamName), "w")
+            seasonResults = open('testfile.txt', 'r')
+
+            for line in seasonResults:
+
+                home, away, result, newline = line.split(',')
+                if home == teamName:
+                    if result == 'H':
+                        beatenTeam.append(away)
+                        win += 1
+                    elif result == 'A':
+                        lostToTeams.append(away)
+                        loss += 1
+                    elif result == 'D':
+                        drewWith.append(away)
+                        draw += 1
+                elif away == teamName:
+                    if result == 'A':
+                        beatenTeam.append(home)
+                        win += 1
+                    elif result == 'H':
+                        lostToTeams.append(home)
+                        loss += 1
+                    elif result == 'D':
+                        drewWith.append(home)
+                        draw += 1
+
+            teamFiles.write("Premier League Statistics 2016/17\n\n")
+            teamFiles.write("Win count = " + str(win) + "\n")
+            teamFiles.write("Loss count = " + str(loss) + "\n")
+            teamFiles.write("Draw Count = " + str(draw) + "\n")
+            teamFiles.write("Beaten Teams = " + str(beatenTeam) + "\n")
+            teamFiles.write("Lost to Teams = " + str(lostToTeams) + "\n")
+            teamFiles.write("Drew with Teams = " + str(drewWith) + "\n")
+
+            win = 0
+            loss = 0
+            draw = 0
+            beatenTeam = []
+            lostToTeams = []
+            drewWith = []
+            teamFiles.close()
