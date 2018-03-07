fd1668517732ab175fb6a994e3ebab45308778cc
PatrickKelly19 <jkjkelly9@gmail.com>
2018-03-07 18:46:48 +0000

    Betting info added into db and txt files

--- Test1.py
+++ Test1.py
@@ -1,53 +1,54 @@
 import pymysql
 
-# Pathtofile = "1617.txt"
-# def WriteToDB(words):
-#     print(words)
 conn = pymysql.connect(host='104.197.152.81', user='root', passwd='root', db='Football')
 cur = conn.cursor()
 
-def InsertTeams(word,word1,word2,word3,word4):
+def InsertTeams(word1,word2,word3,word4,word5,word6,word7,word8,word9,word10,word11,word12,word13,word14):
     conn = pymysql.connect(host='104.197.152.81', user='root', passwd='root', db='Football')
     cur = conn.cursor()
-    cur.execute("insert into PremierLeagueResults (HomeTeam, AwayTeam, HomeScore, AwayScore, Winner) values (%s,%s, %s, %s, %s)", (word,word1,word2,word3,word4))
+    cur.execute("insert into PremierLeagueResultsAndOdds (HomeTeam, AwayTeam, HomeScore, AwayScore, Winner, Bet365Home, Bet365Draw, Bet365Away, BetAndWinHome, BetAndWinDraw, BetAndWinAway, LadbrokesHome, LadbrokesDraw, LadbrokesAway) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (word1,word2,word3,word4,word5,word6,word7,word8,word9,word10,word11,word12,word13,word14))
     conn.commit()
 
-def InsertHA(rowsss, tt):
-    print(rowsss)
-    print(tt)
+thisvalue = ['1718']
 
-thisvalue = ['1617', '1516', '1415', '1314', '1213', '1112', '1011', '0910', '0809', '0708', '0607', '0506', '0304',
-                 '0203', '0102', '0001', '9900', '9899', '9798', '9697', '9596', '9495', '9394']
-
-Teams = ['Arsenal', 'Aston Villa', 'Barnsley', 'Birmingham', 'Blackburn', 'Blackpool', 'Bolton',
-         'Bournemouth', 'Bradford', 'Brighton', 'Burnley', 'Cardiff', 'Charlton', 'Chelsea', 'Coventry',
-         'Crystal Palace', 'Derby', 'Everton', 'Fulham', 'Huddersfield', 'Hull', 'Ipswich', 'Leeds',
-         'Leicester',
-         'Liverpool', 'Man City', 'Man United', 'Middlesbrough', 'Newcastle', 'Norwich', 'Oldham',
-         'Portsmouth',
-         'QPR', 'Reading', 'Sheffield United', 'Sheffield Weds', 'Southampton', 'Stoke', 'Sunderland',
-         'Swansea',
-         'Swindon', 'Tottenham', 'Watford', 'West Brom', 'West Ham', 'Wigan', 'Wimbledon', 'Wolves']
+Teams = ['Arsenal', 'Bournemouth', 'Burnley', 'Brighton', 'Chelsea', 'Crystal Palace', 'Everton',
+         'Huddersfield', 'Leicester', 'Liverpool', 'Man City', 'Man United', 'Newcastle', 'Southampton', 'Stoke',
+         'Swansea', 'Tottenham', 'Watford', 'West Brom', 'West Ham']
 
 homeTeam = []
 awayTeam = []
 homeScore = []
 awayScore = []
 winnerGame = []
+Bet365H = []
+Bet365D = []
+Bet365A = []
+BetAndWinH = []
+BetAndWinD = []
+BetAndWinA = []
+LadbrokesH = []
+LadbrokesD = []
+LadbrokesA = []
 
 for i in thisvalue:
     with open(i+".txt", 'r') as f:
         for line in f:
             for team in Teams:
                 if team in line:
-                    Home, Away, HS, AS, Result, newline = line.split(',')
+                    Home, Away, HS, AS, Result, B365H, B365D, B365A, BWH, BWD, BWA, LBH, LBD, LBA, newline = line.split(',')
                     if Home == team:
                         homeTeam.append(Home)
                         homeScore.append(HS)
+                        Bet365H.append(B365H)
+                        BetAndWinH.append(BWH)
+                        LadbrokesH.append(LBH)
 
                     elif Away == team:
                         awayTeam.append(Away)
                         awayScore.append(AS)
+                        Bet365A.append(B365A)
+                        BetAndWinA.append(BWA)
+                        LadbrokesA.append(LBA)
 
                         if int(AS) > int(HS):
                             winnerGame.append(Away)
@@ -56,23 +57,24 @@ for i in thisvalue:
                         elif int(AS) == int(HS):
                             winnerGame.append("Draw")
 
-for line, line1, line2, line3, line4 in zip([*homeTeam],[*awayTeam],[*homeScore],[*awayScore],[*winnerGame]):
-    i=0
-    print('{} {} {} {} {}'.format(line,line1,line2,line3,line4))
-    InsertTeams(line,line1,line2,line3,line4)
+                        Bet365D.append(B365D)
+                        BetAndWinD.append(BWD)
+                        LadbrokesD.append(LBD)
 
-Arsenal = cur.execute("SELECT * FROM PremierLeagueResults")
+for line1, line2, line3, line4, line5, line6, line7, line8, line9, line10, line11, line12, line13, line14 in zip([*homeTeam],[*awayTeam],[*homeScore],[*awayScore],[*winnerGame],[*Bet365H],[*Bet365D],[*Bet365A],[*BetAndWinH],[*BetAndWinD],[*BetAndWinA],[*LadbrokesH],[*LadbrokesD],[*LadbrokesA]):
+    print('{} {} {} {} {} {} {} {} {} {} {} {} {} {}'.format(line1,line2,line3,line4,line5,line6,line7,line8,line9,line10,line11,line12,line13,line14))
+    InsertTeams(line1,line2,line3,line4,line5,line6,line7,line8,line9,line10,line11,line12,line13,line14)
 
-Prem = ['Arsenal', 'Bournemouth', 'Burnley', 'Brighton', 'Chelsea', 'Crystal Palace', 'Everton',
+Arsenal = cur.execute("SELECT * FROM PremierLeagueResultsAndOdds")
+
+Teams = ['Arsenal', 'Bournemouth', 'Burnley', 'Brighton', 'Chelsea', 'Crystal Palace', 'Everton',
          'Huddersfield', 'Leicester', 'Liverpool', 'Man City', 'Man United', 'Newcastle', 'Southampton', 'Stoke',
          'Swansea', 'Tottenham', 'Watford', 'West Brom', 'West Ham']
 
 ResultSet = cur.fetchall()
-for team in Prem:
+for team in Teams:
     for row in ResultSet:
         if team in row:
-            #InsertHA(row, team)
-            print(team)
-            print(row)
-            cur.execute("insert into " + team.replace(" ","") + "(id, HomeTeam, AwayTeam, HomeScore, AwayScore, Winner) values (%s,%s,%s,%s,%s,%s)",(row))
+            print(str(row))
+            cur.execute("insert into " + team.replace(" ","") + "(id, HomeTeam, AwayTeam, HomeScore, AwayScore, Winner, Bet365Home, Bet365Draw, Bet365Away, BetAndWinHome, BetAndWinDraw, BetAndWinAway, LadbrokesHome, LadbrokesDraw, LadbrokesAway) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",(row))
             conn.commit()
