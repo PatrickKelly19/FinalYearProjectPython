bd9ea07af1e7d1dc81232ebcbdd4af7131ee19fc
PatrickKelly19 <jkjkelly9@gmail.com>
2018-03-07 20:58:03 +0000

    Duplicate issue for insert into db fixed

--- Test1.py
+++ Test1.py
@@ -6,7 +6,9 @@ cur = conn.cursor()
 def InsertTeams(word1,word2,word3,word4,word5,word6,word7,word8,word9,word10,word11,word12,word13,word14):
     conn = pymysql.connect(host='104.197.152.81', user='root', passwd='root', db='Football')
     cur = conn.cursor()
-    cur.execute("insert into PremierLeagueResultsAndOdds (HomeTeam, AwayTeam, HomeScore, AwayScore, Winner, Bet365Home, Bet365Draw, Bet365Away, BetAndWinHome, BetAndWinDraw, BetAndWinAway, LadbrokesHome, LadbrokesDraw, LadbrokesAway) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (word1,word2,word3,word4,word5,word6,word7,word8,word9,word10,word11,word12,word13,word14))
+    cur.execute(
+        "insert into PleaseWorkForEverything (HomeTeam, AwayTeam, HomeScore, AwayScore, Winner, Bet365Home, Bet365Draw, Bet365Away, BetAndWinHome, BetAndWinDraw, BetAndWinAway, LadbrokesHome, LadbrokesDraw, LadbrokesAway) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+        (word1, word2, word3, word4, word5, word6, word7, word8, word9, word10, word11, word12, word13, word14))
     conn.commit()
 
 thisvalue = ['1718']
@@ -65,7 +67,7 @@ for line1, line2, line3, line4, line5, line6, line7, line8, line9, line10, line1
     print('{} {} {} {} {} {} {} {} {} {} {} {} {} {}'.format(line1,line2,line3,line4,line5,line6,line7,line8,line9,line10,line11,line12,line13,line14))
     InsertTeams(line1,line2,line3,line4,line5,line6,line7,line8,line9,line10,line11,line12,line13,line14)
 
-Arsenal = cur.execute("SELECT * FROM PremierLeagueResultsAndOdds")
+Arsenal = cur.execute("SELECT * FROM PleaseWorkForEverything")
 
 Teams = ['Arsenal', 'Bournemouth', 'Burnley', 'Brighton', 'Chelsea', 'Crystal Palace', 'Everton',
          'Huddersfield', 'Leicester', 'Liverpool', 'Man City', 'Man United', 'Newcastle', 'Southampton', 'Stoke',
@@ -75,6 +77,7 @@ ResultSet = cur.fetchall()
 for team in Teams:
     for row in ResultSet:
         if team in row:
-            print(str(row))
-            cur.execute("insert into " + team.replace(" ","") + "(id, HomeTeam, AwayTeam, HomeScore, AwayScore, Winner, Bet365Home, Bet365Draw, Bet365Away, BetAndWinHome, BetAndWinDraw, BetAndWinAway, LadbrokesHome, LadbrokesDraw, LadbrokesAway) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",(row))
+            conn.commit()
+            cur.execute(
+                "DELETE t1 FROM PleaseWorkForEverything t1, PleaseWorkForEverything t2 WHERE t1.HomeTeam = t2.HomeTeam AND t1.AwayTeam = t2.AwayTeam AND t1.HomeScore = t2.HomeScore AND t1.AwayScore = t2.AwayScore AND t1.id > t2.id")
             conn.commit()
